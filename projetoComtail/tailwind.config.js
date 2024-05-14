9f57ac961aecfe7e807835d01838be688dad315d
maximos34 <44528369+maximos34@users.noreply.github.com>
2024-05-14 11:18:13 -0300

    começando projeto com tail

---
+++ projetoComtail/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        backgroundColor:'#FED7AA',
+        seconderyBackgroundColor:'#995000',
+      }
+    },
+  },
+  plugins: [],
+}
